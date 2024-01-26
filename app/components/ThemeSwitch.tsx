aa24a042dd83d50bb36fef9affc8c086b3adb47e
ESE-MONDAY <mondayese8@gmail.com>
2024-01-26 20:58:06 +0100

    changes

--- app/components/ThemeSwitch.tsx
+++ app/components/ThemeSwitch.tsx
@@ -13,10 +13,10 @@ const  ThemeSwitch = () => {
  return (
    <div className='flex flex-col gap-4 '>
     <button className='flex flex-col gap-4 p-2 bg-white dark:bg-[#282828]  rounded-full transition-colors duration-300'  onClick={toggleTheme}>
-        <span className={`flex items-center justify-center p-2 rounded-full transition-colors duration-100 ${theme === "dark" ? "" : "bg-[#34CAA5]"}`}>
+        <span className={`flex items-center justify-center p-2 rounded-full transition-colors duration-100 ${theme === "light" && "bg-[#34CAA5]"}`}>
         <FiSun size={24} className={`${theme === "dark" ? "text-[#B2ABAB]" : "text-white"}`} />
         </span>
-        <span className={`flex items-center justify-center p-2 rounded-full transition-colors duration-100 ${theme === "light" ? "" : "bg-[#34CAA5]"}  `}>
+        <span className={`flex items-center justify-center p-2 rounded-full transition-colors duration-100 ${theme === "dark" && "bg-[#34CAA5]" }  `}>
         <FiMoon size={24} className={`${theme === "light" ? "text-[#B2ABAB]" : "text-white"}`}  />
         </span>
         
